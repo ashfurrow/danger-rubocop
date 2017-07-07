5641a4d873657f41e0b31304a5260b55814ac80d
javierg <javierg@amcoonline.net>
2017-07-06 21:54:34 -0700

    syntax errors

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -72,7 +72,7 @@ module Danger
     def report_failures(offending_files)
       offending_files.each do |file|
         file['offenses'].each do |offense|
-          fail "#{file['path']} | #{offense['location']['line'] | offense['message']}"
+          fail "#{file['path']} | #{offense['location']['line']} | #{offense['message']}"
         end
       end
     end
