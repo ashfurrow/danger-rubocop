0dbbed985ecf09eb1d0522fecc2a891de42f5b3e
Huy Vo <huyvohcmc@gmail.com>
2018-06-07 13:21:05 +0700

    Clean up danger plugin and bump version

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -1,32 +1,25 @@
 module Danger
   class DangerRubocop < Plugin
-    def lint(files = nil, whitelist = [], cops_to_ignore = [])
-      @whitelist = whitelist
-      rubocop_offending_files = offending_files(files, cops_to_ignore)
+    def lint(files = nil)
+      rubocop_offending_files = offending_files(files)
       return unless rubocop_offending_files.any?
-      markdown offenses_message(rubocop_offending_files)
-    end
-
-    def offending_files(files = nil, cops_to_ignore = [])
-      files_to_lint = fetch_files_to_lint(files)
-      rubocop(files_to_lint, cops_to_ignore)
+      [markdown(offenses_message(rubocop_offending_files)), rubocop_offending_files]
     end
 
     private
+
+    def offending_files(files = nil)
+      files_to_lint = fetch_files_to_lint(files)
+      rubocop(files_to_lint)
+    end
 
     def fetch_files_to_lint(files = nil)
       @files_to_lint ||= (files ? Dir.glob(files) : (git.modified_files + git.added_files))
     end
 
-    def rubocop(files_to_lint, cops_to_ignore = [])
+    def rubocop(files_to_lint)
       rubocop_output = `#{'bundle exec ' if need_bundler?}rubocop -f json #{files_to_lint.join(' ')}`
-
-      JSON.parse(rubocop_output)['files'].map do |file|
-        file['offenses'].reject! do |offense|
-          cops_to_ignore.include?(offense['cop_name'])
-        end
-        file unless file['offenses'].empty?
-      end.compact
+      JSON.parse(rubocop_output)['files'].select { |f| f['offenses'].any? }
     end
 
     def offenses_message(offending_files)
@@ -34,12 +27,11 @@ module Danger
 
       message = "### Rubocop violations\n\n"
       table = Terminal::Table.new(
-        headings: %w(Required File Line Reason),
+        headings: %w(File Line Reason),
         style: { border_i: '|' },
         rows: offending_files.flat_map do |file|
           file['offenses'].map do |offense|
             [
-              required?(file['path']) ? ':x:' : '',
               file['path'],
               offense['location']['line'],
               offense['message']
@@ -48,10 +40,6 @@ module Danger
         end
       ).to_s
       message + table.split("\n")[1..-2].join("\n")
-    end
-
-    def required?(file_path)
-      (@whitelist + git.added_files).include?(file_path)
     end
 
     def need_bundler?
